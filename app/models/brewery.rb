03474a17e7067482c55ed54b83ef77777b3748a6
erik ruthven <erik@emergeinteractive.com>
2014-05-14 14:02:37 -0700

    actually plotted markers

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -12,12 +12,12 @@ class Brewery < ActiveRecord::Base
 				counter += 1
 				# puts i.at_xpath("id").text + ' ' + i.at_xpath("name").text + ' ' + i.at_xpath("status").text
 				coords = Nokogiri::XML(open("http://beermapping.com/webservice/locmap/05a1a701dca974e16978f0c9027709b1/#{i.at_xpath("id").text}"))
-				puts coords.xpath("//name").text + ' ' + coords.xpath("//lat").text + ' ' + coords.xpath("//lng").text
+				# puts coords.xpath("//name").text + ' ' + coords.xpath("//lat").text + ' ' + coords.xpath("//lng").text
 				Brewery.create({location_id: i.at_xpath("id").text,name: i.at_xpath("name").text,lat: coords.xpath("//lat").text,long: coords.xpath("//lng").text})
 			end
 		end
-		puts "----------------------"
-		puts "total " + counter.to_s
+		# puts "----------------------"
+		# puts "total " + counter.to_s
 		# get lat long http://beermapping.com/webservice/locmap/API_KEY/<location id from above>
 
 		# doc2 = Nokogiri::XML(open("http://beermapping.com/webservice/locmap/05a1a701dca974e16978f0c9027709b1/14556"))
