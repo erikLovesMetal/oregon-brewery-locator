503d9abf8d894951677bf3ec814091e2cf0b82b4
erik ruthven <erik@emergeinteractive.com>
2014-05-21 23:47:31 -0700

    added the modal popup on marker click

--- app/assets/javascripts/controllers/main/mapIndexCtrl.js
+++ app/assets/javascripts/controllers/main/mapIndexCtrl.js
@@ -11,7 +11,6 @@ app.controller('IndexCtrl', ['$scope','$http','leafletData','GeolocationService'
       closeButton: true,
       position: "left"
     });
-    console.log(map);
     map._layersMaxZoom=20;
     map.options.maxZoom=20;
     map.addControl($scope.sidebar);
@@ -22,14 +21,31 @@ app.controller('IndexCtrl', ['$scope','$http','leafletData','GeolocationService'
     $scope.sidebar.toggle();
   }
 
-  // when we have our coords then plot a marker for us
+  //when we have our coords then plot a marker for us
   $scope.$on('leafletDirectiveMap.locationfound', function(event){
         $("#loading").hide();
         $scope.eventDetected = "location!";
         console.log($scope.centerPoint.lat);
         console.log($scope.centerPoint.lng);
-        $scope.results['x']={lat:parseFloat($scope.centerPoint.lat),lng: parseFloat($scope.centerPoint.lng),message: 'You Are Here',focus:true};
+        $scope.results['x']={lat:parseFloat($scope.centerPoint.lat),lng: parseFloat($scope.centerPoint.lng),message: "You Are Here",focus:true};
     });
+
+  $scope.events = {
+    markers: {
+      enable: ['click'],
+      logic: 'emit'
+    }
+  };
+
+  // add listeners for specific markers click event then open populated modal
+  $scope.$on('leafletDirectiveMarker.click',function(event,args){
+    var content = "<table class='table table-striped table-bordered table-condensed'>" + "<tr><th>Division</th><td>" + $scope.markers[args.markerName].message + "</td></tr>" + "<tr><th>Line</th><td>" + $scope.markers[args.markerName].message + "</td></tr>" + "<table>";
+    $("#feature-title").html($scope.markers[args.markerName].message);
+    $("#feature-info").html(content);
+    $("#featureModal").modal("show");
+    console.log($scope.markers[args.markerName]);
+  });
+
 
   // plot some breweries!...
   $http({method: 'GET', url: 'map/getBreweryCoords'}).
@@ -38,7 +54,7 @@ app.controller('IndexCtrl', ['$scope','$http','leafletData','GeolocationService'
       // when the response is available
       $(data).each(function(index){
         if( this.lat != 0.0 && this.long != 0.0){
-          $scope.results['m' + index]={lat:parseFloat(this.lat),lng: parseFloat(this.long),name: this.name};
+          $scope.results['m' + index]={lat:parseFloat(this.lat),lng: parseFloat(this.long),message: this.name};
         }
       });
     }).
---
+++ app/models/strings.rb
@@ -0,0 +1,1 @@
+puts "suck my balls"
