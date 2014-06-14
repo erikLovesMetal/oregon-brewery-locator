46cda2714a34dbe35d3cc9728416dbd2bcc1e57a
erik ruthven <erik@emergeinteractive.com>
2014-06-13 21:46:14 -0700

    playing with geocoder settings

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -2,11 +2,11 @@ Geocoder.configure(
   # geocoding service
   lookup: :google,
 
-  :http_proxy => ENV['QUOTAGUARD_URL'],
-  :timeout => 5,
+  # :http_proxy => ENV['QUOTAGUARD_URL'],
+  # :timeout => 50,
 
   # geocoding service request timeout (in seconds)
-  # timeout: 3,
+  timeout: 3,
 
   # default units
   units: :km
