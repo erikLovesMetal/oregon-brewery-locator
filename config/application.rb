e2576e95f83f0fb92d171daa618cc680b821cbb3
erik ruthven <erik@emergeinteractive.com>
2014-06-15 16:56:50 -0700

    added breweries js to precompile list, its getting 404 on heroku

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module BreweryLocator
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.precompile += [
-      'map.js'
+      'map.js','breweries.js'
     ]
     # config.assets.paths << "/app/assets/stylesheets/leaflet/fonts"
     # config.assets.paths << "#{Rails}/vendor/assets/fonts"
